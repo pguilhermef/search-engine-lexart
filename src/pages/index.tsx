2cbb582ff42b0a35875b489f9431b2287511c384
pguilhermef <pguilhermesantos@hotmail.com>
2023-04-14 16:27:53 -0300

    feat: Creates front-end page to search products

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,124 +1,157 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import IProduct from "@/interfaces/IProduct";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react"
 
-const inter = Inter({ subsets: ['latin'] })
+const categories = ['Geladeira', 'TV', 'Celular'];
+const sellersOptions = ['Todas', 'Mercado Livre', 'Buscapé'];
 
 export default function Home() {
+  const [products, setProducts] = useState([]) 
+  const [seller, setSeller] = useState('');
+  const [category, setCategory] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+  const [isSearched, setIsSearched] = useState(false)
+
+  useEffect(() => {
+    if (isSearched) {
+      setIsSearched(false)
+
+      fetch('/api/products')
+        .then(response => response.json())
+        .then(products => setProducts(products.data))
+    }
+  }, [isSearched])
+
+  const handleWebsiteChange = (event: React.ChangeEvent<EventTarget | HTMLInputElement>) => {
+    const target = event.target as HTMLInputElement;
+    setSeller(target.value);
+  };
+
+  const handleCategoryChange = (event: React.ChangeEvent<EventTarget | HTMLInputElement>) => {
+    const target = event.target as HTMLInputElement;
+    setCategory(target.value);
+  };
+
+  const handleSearchChange = (event: React.ChangeEvent<EventTarget | HTMLInputElement>) => {
+    const target = event.target as HTMLInputElement;
+    setSearchQuery(target.value);
+  };
+
+  const handleSearchSubmit = () => {
+    setIsSearched(!isSearched)
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="h-screen w-screen flex justify-center items-center">
+      <div className="flex flex-col justify-start items-center bg-white border-4 border-black h-5/6 w-3/4 max-w-5xl">
+        <div className="flex items-center my-10 gap-x-4">
+          <div>
+            <label htmlFor="website" className="sr-only">
+              Website
+            </label>
+            <select
+              id="website"
+              name="website"
+              value={seller}
+              onChange={handleWebsiteChange}
+              className="rounded-md border-gray-300 shadow-sm text-white bg-indigo-800 hover:bg-indigo-600 px-4 py-2"
+            >
+              <option value="" disabled hidden>
+                Web
+              </option>
+              {sellersOptions.map((option) => (
+                <option key={option} value={option} className="text-black bg-white hover:bg-slate-400">
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <label htmlFor="category" className="sr-only">
+              Category
+            </label>
+            <select
+              id="category"
+              name="category"
+              value={category}
+              onChange={handleCategoryChange}
+              className="rounded-md border-gray-300 shadow-sm text-white bg-indigo-800 hover:bg-indigo-600 px-4 py-2"
+            >
+              <option value="" disabled hidden>
+                Categories
+              </option>
+              {categories.map((option) => (
+                <option key={option} value={option} className="text-black bg-white hover:bg-slate-400">
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <input
+              type="text"
+              name="search"
+              placeholder="Search products"
+              value={searchQuery}
+              onChange={handleSearchChange}
+              className="rounded-md border-2 border-black shadow-sm px-4 py-2"
             />
-          </a>
+          </div>
+          <div>
+            <button
+              type="button"
+              onClick={handleSearchSubmit}
+              className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-800 hover:bg-indigo-600 focus:outline-none"
+            >
+              Search
+            </button>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        { products.length > 0 ? (
+          <div className="flex justify-center flex-wrap max-w-2xl h-4/6 overflow-y-scroll">
+          {products.map((product: IProduct) => (
+            <div key={product.id} className='my-5'>
+              <div className='flex justify-center items-center'>
+                {/* Image */}
+                <div>
+                  <Image src={product.photo} alt={product.description} width={200} height={200}/>
+                </div>
+                {/* Infos */}
+                <div className='flex flex-col items-start mx-5 gap-y-4'>
+                  <div className='font-semibold text-xl'>
+                    {/* here catch the first two words of name */}
+                    {product.title.split(" ").slice(0, 4).join(" ")}
+                  </div>
+                  <div className='max-w-[20rem]'>
+                    {
+                    product.description.length > 100 ?
+                    `${product.description.slice(0, 100)}...` : product.description}
+                  </div>
+                  <div className='font-semibold text-xl'>
+                    {product.price}
+                  </div>
+                </div>
+                {/* Button */}
+                <div>
+                  <Link href={product.link} target='_blank'>
+                    <div
+                      className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-800 hover:bg-indigo-600 focus:outline-none"
+                      >
+                      Search
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        ) : (
+          <div>
+            Pesquise por um produto!
+          </div>
+        )}
       </div>
     </main>
-  )
+  );
 }
