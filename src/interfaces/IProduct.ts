1461f4fd3230cb53c0eed1e8eda3eb5176333022
pguilhermef <pguilhermesantos@hotmail.com>
2023-04-14 16:15:30 -0300

    feat: Creates product interface

---
+++ src/interfaces/IProduct.ts
@@ -0,0 +1,10 @@
+export default interface IProduct {
+  id: number
+  title: string,
+  photo: string,
+  description: string,
+  price: string,
+  link: string,
+  seller: string,
+  category: string,
+}
