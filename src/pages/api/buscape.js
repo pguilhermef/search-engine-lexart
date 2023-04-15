86c89a308c2d7698d334c6fe127065f9695d4997
pguilhermef <pguilhermesantos@hotmail.com>
2023-04-15 15:40:08 -0300

    refactor: Optimitation in buscape.ts code

--- src/pages/api/buscape.js
+++ src/pages/api/buscape.js
@@ -6,28 +6,22 @@ const prisma = new PrismaClient()
 
 
 async function buscapeScraping(categorieToSearch, searchInput) {
+  const buscapeUrl = 'https://www.buscape.com.br/';
   const products = [];
   let count = 0;
 
-  const buscapeUrl = 'https://www.buscape.com.br/';
 
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-
-  await page.setViewport({
-    width: 1200,
-    height: 800,
-  });
+  await page.setViewport({ width: 1200, height: 800 });
 
   await page.goto(buscapeUrl, { waitUntil: 'domcontentloaded' })
-
   await page.type('.AutoCompleteStyle_input__HG105', searchInput);
   await page.click('.AutoCompleteStyle_submitButton__GkxPO')
   await page.waitForNavigation({ waitUntil: 'domcontentloaded' })
   await page.click('.Dropdown_Dropdown__yBuX5');
   
   const dropdownCategory = await page.$eval('.Dropdown_DropdownHeader__N3Zqc', element => element.innerText)
-  
   if (dropdownCategory !== categorieToSearch) {
     await page.click(`[title="${categorieToSearch}"]`);
     await page.waitForNavigation({ waitUntil: 'domcontentloaded' })
@@ -51,63 +45,62 @@ async function buscapeScraping(categorieToSearch, searchInput) {
     const description = await page.$$eval('.Description_GradientOverflow__ncmUo > div > p', elements => elements.map(element => element.innerText))
     const price = await page.$eval('.Price_ValueContainer__1U9ia', element => element.innerText)
 
-    const obj = {
-      title,
+    const product = {
+      title: title.split(" ").slice(0, 4).join(" "),
       photo,
-      description: "testando",
+      description: `${description.join(' ').slice(0, 150)}...`,
       price,
       link,
       seller: 'buscape',
       category: categorieToSearch,
     }
 
-    products.push(obj)
+    products.push(product)
 
     count++
   }
 
   await browser.close()
 
-  return products
+  return { message: null, products }
 }
 
 export default async function handler(req, res) {
   const { categorieToSearch, searchInput } = req.query;
-  const { method } = req;
 
   if (!categorieToSearch || !searchInput) {
-    res.status(400).json({ error: 'Parâmetros faltando na requisição.' });
-    return;
+    return res.status(400).json({ message: 'Parâmetros faltando na requisição.' });
   }
 
-  if(method === 'GET'){
-    let products = await prisma.product.findMany({
+  try {
+    const productsInDatabase = await prisma.product.findMany({
       where: {
         category: categorieToSearch,
-        OR: {
-          title: {
-            contains: searchInput,
-          },
-          description: {
-            contains: searchInput,
-          }
-        }
-      }
-    })
+        title: {
+          contains: searchInput,
+        },
+      },
+    });
 
-    if(!products.length){
-      console.log('Buscando produtos...');
-      products = await buscapeScraping(categorieToSearch, searchInput)
-
-      let promises = products.map((p) => prisma.product.create({
-        data: p
-      }))
-
-      await Promise.all(promises)
-
-      console.log(promises);
+    if (productsInDatabase.length > 0) {
+      return res.status(200).json({ message: null, products: productsInDatabase });
     }
 
-    return res.status(200).json(products)
+    console.log('Buscando produtos...');
+    const productsInScrap = await buscapeScraping(categorieToSearch, searchInput);
+
+    if (productsInScrap.message) {
+      return res.status(400).json({ message: productsInScrap.message, products: [] });
+    }
+
+    const createdProducts = await prisma.product.createMany({
+      data: productsInScrap.products,
+    });
+
+    console.log(`${createdProducts.count} produtos criados.`);
+    return res.status(200).json({ message: null, products: productsInScrap.products });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Erro interno do servidor.' });
   }
 }
