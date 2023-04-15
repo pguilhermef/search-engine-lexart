306ad1e32304ceae1f4b6e92d27015401c072fee
pguilhermef <pguilhermesantos@hotmail.com>
2023-04-15 12:59:00 -0300

    feat: Web sriping results are now saved to the database

--- src/pages/api/buscape.js
+++ src/pages/api/buscape.js
@@ -1,20 +1,17 @@
+import { PrismaClient } from '@prisma/client';
+
 const puppeteer = require('puppeteer');
 
-const buscapeUrl = 'https://www.buscape.com.br/';
+const prisma = new PrismaClient()
 
-let count = 0;
-const products = [];
 
-export default async function handler(req, res) {
-  const { categorieToSearch, searchInput } = req.query;
+async function buscapeScraping(categorieToSearch, searchInput) {
+  const products = [];
+  let count = 0;
 
-  if (!categorieToSearch || !searchInput) {
-    res.status(400).json({ error: 'Parâmetros faltando na requisição.' });
-    return;
-  }
+  const buscapeUrl = 'https://www.buscape.com.br/';
 
   const browser = await puppeteer.launch()
-
   const page = await browser.newPage()
 
   await page.setViewport({
@@ -48,21 +45,20 @@ export default async function handler(req, res) {
     console.log('Produto', count);
 
     await page.goto(link, { waitUntil: 'domcontentloaded' })
+
     const title = await page.$eval('.Title_Name__qQvSr', element => element.innerText)
-    
-    const name = await page.$eval('.Title_Name__qQvSr', element => element.innerText)
     const photo = await page.$eval('.swiper-slide > img ', element => element.src)
     const description = await page.$$eval('.Description_GradientOverflow__ncmUo > div > p', elements => elements.map(element => element.innerText))
     const price = await page.$eval('.Price_ValueContainer__1U9ia', element => element.innerText)
-    const website = link
 
     const obj = {
+      title,
       photo,
-      name,
-      description,
-      category: categorieToSearch,
+      description: "testando",
       price,
-      website,
+      link,
+      seller: 'buscape',
+      category: categorieToSearch,
     }
 
     products.push(obj)
@@ -71,6 +67,47 @@ export default async function handler(req, res) {
   }
 
   await browser.close()
-  
-  res.status(200).json({ products });
+
+  return products
+}
+
+export default async function handler(req, res) {
+  const { categorieToSearch, searchInput } = req.query;
+  const { method } = req;
+
+  if (!categorieToSearch || !searchInput) {
+    res.status(400).json({ error: 'Parâmetros faltando na requisição.' });
+    return;
+  }
+
+  if(method === 'GET'){
+    let products = await prisma.product.findMany({
+      where: {
+        category: categorieToSearch,
+        OR: {
+          title: {
+            contains: searchInput,
+          },
+          description: {
+            contains: searchInput,
+          }
+        }
+      }
+    })
+
+    if(!products.length){
+      console.log('Buscando produtos...');
+      products = await buscapeScraping(categorieToSearch, searchInput)
+
+      let promises = products.map((p) => prisma.product.create({
+        data: p
+      }))
+
+      await Promise.all(promises)
+
+      console.log(promises);
+    }
+
+    return res.status(200).json(products)
+  }
 }
