43ccd8c0aa4325ba82b5ee3ab9737f50c8f0cb49
pguilhermef <pguilhermesantos@hotmail.com>
2023-04-15 15:58:39 -0300

    feat: Creates a message component and interface to show status of search

---
+++ src/components/DisplayMessage.tsx
@@ -0,0 +1,22 @@
+import IMessages from '@/interfaces/IMessages';
+import React, { useState } from 'react'
+
+type Props = {
+  message: string
+}
+
+const messages: IMessages = {
+  'initial': 'Search for a product!',
+  'searching': 'Searching...',
+  'errorInSearch': 'Error during search, please try again or try a different term'
+};
+
+export default function DisplayMessage({message}: Props) {
+  
+
+  return (
+    <div className='mt-40 p-5'>
+      {messages[message]}
+    </div>
+  )
+} 
