0f30f575062d664dec27509b6ce0d20e48b6665a
pguilhermef <pguilhermesantos@hotmail.com>
2023-04-17 20:33:39 -0300

    refactor: Reduces length of buscapeScraping function to deploy on vercel

--- src/utils/buscapeScrap.js
+++ src/utils/buscapeScrap.js
@@ -1,75 +1,56 @@
 const chromium = require('chrome-aws-lambda');
+const chrome = process.env.AWS_LAMBDA_FUNCTION_VERSION ? require('chrome-aws-lambda') : {};
 
-let chrome = {}
-let puppeteer;
+const tvId = 3;
+const cellId = 7;
+const refrigeratorId = 8;
 
-if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-  chrome = require('chrome-aws-lambda')
-  puppeteer = require("puppeteer-core")
-} else {
-  puppeteer = require("puppeteer")
-}
+const categoryIds = {
+  Geladeira: refrigeratorId,
+  TV: tvId,
+  Celular: cellId,
+};
+
+const defaultViewport = { width: 1200, height: 800 };
+const args = [...chrome.args, "--hide-scrollbars", "disable-web-security"];
 
 export default async function buscapeScraping(categorieToSearch, searchInput) {
   const products = [];
   let count = 0;
 
-  let options = {}
+  const categoryId = categoryIds[categorieToSearch];
 
-  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    options = {
-      args: [...chrome.args, "--hide-scrollbars", "disable-web-security"],
-      defaultViewport: ({ width: 1200, height: 800 }),
-      executablePath: await chromium.executablePath,
-      headless: true,
-      ignoreHTTPSErrors: true,
-    };
-  }
+  const options = process.env.AWS_LAMBDA_FUNCTION_VERSION
+    ? {
+        args,
+        defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+      }
+    : {};
 
-  let categoryId = 0;
-  const tvId = 3;
-  const cellId = 7;
-  const refrigeratorId = 8;
+  const buscapeUrl = `https://www.buscape.com.br/search?q=${searchInput}&refinements%5B0%5D%5Bid%5D=categoryId&refinements%5B0%5D%5Bvalues%5D%5B0%5D=${categoryId}&isDealsPage=false`;
 
-  switch (categorieToSearch) {
-    case 'Geladeira':
-      categoryId = refrigeratorId;
-      break;
-    case 'TV':
-      categoryId = tvId;
-      break;
-    case 'Celular':
-      categoryId = cellId;
-      break;
-    default:
-      console.log('Error on category search');
-  }
+  const puppeteer = process.env.AWS_LAMBDA_FUNCTION_VERSION ? require("puppeteer-core") : require("puppeteer");
+  const browser = await puppeteer.launch(options);
 
-  const buscapeUrl = `https://www.buscape.com.br/search?q=${searchInput}&refinements%5B0%5D%5Bid%5D=categoryId&refinements%5B0%5D%5Bvalues%5D%5B0%5D=${categoryId}&isDealsPage=false`
+  const page = await browser.newPage();
+  await page.goto(buscapeUrl, { waitUntil: 'domcontentloaded' });
 
-  let browser = await puppeteer.launch(options)
+  const links = await page.$$eval('.SearchCard_ProductCard_Inner__7JhKb', el => el.map(link => link.href));
 
-  let page = await browser.newPage();
-
-  console.log('alô, fala comigo');
-
-  await page.goto(buscapeUrl, { waitUntil: 'domcontentloaded' })
-
-  const links = await page.$$eval('.SearchCard_ProductCard_Inner__7JhKb', el => el.map(
-    link => link.href
-  ))
-
-  for(const link of links){
+  for (const link of links) {
     if (link === links[0] || count === 10) {
       continue;
     }
 
-    await page.goto(link, { waitUntil: 'domcontentloaded' })
+    await page.goto(link, { waitUntil: 'domcontentloaded' });
 
-    const title = await page.$eval('.Title_Name__qQvSr', element => element.innerText)
-    const photo = await page.$eval('.swiper-slide > img ', element => element.src)
-    const description = await page.$$eval('.Description_GradientOverflow__ncmUo > div > p', elements => elements.map(element => element.innerText))
-    const price = await page.$eval('.Price_ValueContainer__1U9ia', element => element.innerText)
+    const title = await page.$eval('.Title_Name__qQvSr', element => element.innerText);
+    const photo = await page.$eval('.swiper-slide > img ', element => element.src);
+    const description = await page.$$eval('.Description_GradientOverflow__ncmUo > div > p', elements => elements.map(element => element.innerText));
+    const price = await page.$eval('.Price_ValueContainer__1U9ia', element => element.innerText);
 
     const product = {
       title: title.split(' ').slice(0, 4).join(' '),
@@ -79,16 +60,15 @@ export default async function buscapeScraping(categorieToSearch, searchInput) {
       link,
       seller: 'buscape',
       category: categorieToSearch,
-    }
+    };
 
-    products.push(product)
-
-    count++
+    products.push(product);
+    count++;
   }
 
-  console.log(`${count} products founded!`)
+  console.log(`${count} products found!`);
 
-  await browser.close()
+  await browser.close();
 
-  return { message: null, products }
+  return { message: null, products };
 }
